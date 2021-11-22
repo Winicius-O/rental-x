cf5f1fa24142caa1b31ce365b3290ae361de6f6f
Winicius-O <winiciusoliveira3@gmail.com>
2021-11-22 14:58:49 -0300

    created CreateUserController and the route for its use

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import { categorieRoutes } from "./categories.routes";
 import { specificationRoutes } from "./specifications.routes";
+import { userRoutes } from "./users.routes";
 
 const router = Router();
 
 router.use("/categories", categorieRoutes);
 router.use("/specifications", specificationRoutes);
+router.use("/users", userRoutes);
 
 export { router };
