42407a86b595b69cbc40d4593c6b17cfeda703fa
Winicius-O <winiciusoliveira3@gmail.com>
2021-11-22 17:40:48 -0300

    add bcrypt package, secured the user password with hash, prevent the user from using an email more than once on post route

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -1,7 +1,9 @@
 import { IUsersRepositoryDTO } from "../dtos/IUsersRepositoryDTO";
+import { User } from "../entities/User";
 
 interface IUsersRepository {
     create(data: IUsersRepositoryDTO): Promise<void>;
+    findByEmail(email: string): Promise<User>;
 }
 
 export { IUsersRepository };
