9e30f47e2c469b303e12f709dd92e072181df23a
Winicius-O <winiciusoliveira3@gmail.com>
2021-12-03 18:50:02 -0300

    utility function to delete a file

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,5 +1,7 @@
 import { inject, injectable } from "tsyringe";
+
 import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { deleteFile } from "../../../../utils/file";
 
 interface IRequest {
     id: string;
@@ -15,6 +17,10 @@ class UpdateUserAvatarUseCase {
 
     async execute({ id, avatarPath }: IRequest): Promise<void> {
         const user = await this.usersRepository.findById(id);
+
+        if (user.avatar) {
+            await deleteFile(`./tmp/avatar/${user.avatar}`);
+        }
 
         user.avatar = avatarPath;
 
